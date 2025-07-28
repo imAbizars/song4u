4ccfa8caa96ed2561cc00503b25bc83dd6aa0ea8
gyozasushi <denisabizar54@gmail.com>
2025-07-28 14:48:04 +0700

    style:edit margin for marquee container

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ export default function Home(){
             Show All Message
           </Link>
       </div>
-      <div className="mt-10 marquee-container max-h-50 ">
+      <div className="mt-20 marquee-container max-h-50 ">
       <div className="marquee-content gap-5 ">
           {[...messages, ...messages].map((msg, index) => (
             <MessageCard className="bg-white text-left border border-color  rounded-lg overflow-hidden  w-full p-2" key={`${msg.id}-${index}`} msg={msg} />
